e08ba4079c6ec45b92864226a7bdc9c178e5dd39
MAHDI <mhdiiranii@gmail.com>
2024-11-28 21:51:15 +0330

    use sesshion hok

---
+++ app/hook/UseSession.js
@@ -0,0 +1,35 @@
+'use client'
+
+import { useEffect, useState } from 'react';
+
+export function useSession() {
+  const [session, setSession] = useState(null);
+  const [status,setStatus] = useState('authenticated')
+
+  useEffect(() => {
+    const token = document.cookie.split('=')
+    console.log(token)
+    if (token[1]) {
+      fetch('/api/users/validate', {
+        method:"POST",
+        headers:{"content-type":"appliacation/json"},
+        body:JSON.stringify({token:token[1]})
+      })
+        .then((res) => res.json())
+        .then((data) => {
+          console.log(data.user)
+          if (data.valid) {
+            setSession(data.user);
+            setStatus(data.status);
+          } else {
+            setSession(null);
+            setStatus(data.status)
+          }
+        });
+    }else{
+      setStatus('unauthenticated')
+    }
+  }, []);
+
+  return {session,status};
+}
